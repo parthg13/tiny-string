e106a220566f606d479a418cfe3dd889a17b5324
Parth Gupta <parth131197@gmail.com>
2022-04-07 12:43:10 +0530

    Bug fix

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 module.exports = function tiny(str){
     if(typeof str !== 'string') throw new TypeError('Pass a string value');
-    return '_'.str.replace(/\s/g, '')+'_';
+    return '_'+str.replace(/\s/g, '')+'_';
 }
